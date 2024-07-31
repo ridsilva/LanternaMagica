6787f7b102f5847edee71d0f7fed4209edc83f78
ridsilva <rid.silva@campus.fct.unl.pt>
2024-07-31 20:58:26 +0100

    minor adjustments

--- js/flood.js
+++ js/flood.js
@@ -47,6 +47,6 @@ function floodFill(x,y,colour) {
 
 function colorsMatch(c1,c2) {
     return(
-      c1[0] ===c2[0] && c1[1] ===c2[1] && c1[2] ===c2[2]
+      c1[0] ===c2[0] && c1[1] ===c2[1] && c1[2] ===c2[2] //&& c1[3] ===c2[3] 
     );
   }

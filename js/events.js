f4fbce3481545c7b2568adb432f8d2bdb16b5b1a
ridsilva <rid.silva@campus.fct.unl.pt>
2024-07-31 18:33:16 +0100

    Added scripts and images
    Palette and events.

---
+++ js/events.js
@@ -0,0 +1,125 @@
+let intervalId;
+
+const message = {
+    pt: "Se a paleta for mudada, todo o trabalho realizado vai ser desfeito!",
+    en: "If you change the palette, the drawing will be reset!"
+}
+
+/* Event Listeners */  
+  //Brush
+  pen.addEventListener("click",function(){
+    isEraser = false;
+    if(mode != "projector" && mode != "initial"){
+      mode = "free";
+      currentShape = new Shape("free");
+      
+    }
+  });
+  
+  //Eraser
+  eraser.addEventListener("click",function(){
+      isEraser = true;
+    if(mode != "projector" && mode!= "initial"){
+      mode = "eraser";
+      currentShape = new Shape("eraser");
+
+    }
+
+  });
+  
+  //Change Palette
+  changePalette.addEventListener("click", function(){
+
+    if(mode != "projector"){
+      let computedStyle = window.getComputedStyle(colorPalettePopup);
+      mode = "palette";
+      if (computedStyle.display === "none") {
+          colorPalettePopup.style.display = "block";
+          document.getElementById('paletteContainer').classList.add("d-none");
+          isInPopUp = true;
+
+          let mess = message[currentLanguage];
+          alert(mess)
+    
+          let paletteOptions = document.querySelectorAll('.palette-option');
+    
+          paletteOptions.forEach(function(option) {
+            option.addEventListener('click', function() {
+    
+              let paletteName = option.querySelectorAll('.palette-label')[0].childNodes[0].textContent;
+
+              if(currentPaletteName != paletteName){
+                pg.clear();
+                changeBackgroundImage(imgName);
+              }
+
+              updatePalette(paletteName);
+              colorPalettePopup.style.display = 'none';
+              isInPopUp = false;
+              });
+        });
+          
+      } else {
+          colorPalettePopup.style.display = "none";
+          isInPopUp = false;
+      }
+    }
+  })
+  
+  //Bucket
+  bucket.addEventListener("click",function(){
+    isEraser = false;
+    if(mode != "projector" && mode !="initial"){
+      mode = "fill";
+      currentShape = new Shape("fill");
+    }
+  });
+  
+  //Brush Size
+  lineWeight.addEventListener("change",function(){
+    if(mode != "projector"){
+      pg.strokeWeight(lineWeight.value);
+    }
+
+   });
+
+  //Undo
+  undo.addEventListener("click", function(){
+    try{
+      if(mode != "projector" && mode!= "initial"){
+        let aux = st.undo();
+
+        if(aux != -1){
+          pg.image(aux, 0,0);
+        }
+      }
+
+    } catch (error) {
+      if (error.message === "Empty undoStack") {
+
+      } else {
+        console.error(error);
+      }
+    }
+  })
+
+  //Redo
+  redo.addEventListener("click", function(){
+    try{
+      if(mode != "projector" && mode != "initial"){
+        let aux = st.redo();
+
+        pg.image(aux,0,0);
+      }
+
+
+    } catch(error){
+      if (error.message === "Empty redoStack") {
+
+      } else {
+        console.error(error);
+      }
+    }
+
+  })
+  /* This ends here*/
---
+++ js/palette.js
@@ -0,0 +1,84 @@
+let currentPalette;
+
+//Palettes
+const paletteLapierre = ['rgba(46,54,51,255)', 'rgba(123,66,4,255)', 'rgba(131,192,52,255)', 'rgba(24,82,244,255)',
+    'rgba(253,102,171,255)', 'rgba(241,220,11,255)'];
+   
+const paletteEFNewton = ['rgba(34,33,39,255)', 'rgba(78,67,50,255)', 'rgba(157,194,161,255)', 'rgba(3,155,216,255)',
+    'rgba(252,69,107,255)', 'rgba(210,156,123,255)', 'rgba(212,73,0,255)', 'rgba(252,248,58,255)'];
+   
+const paletteCarpenterWestley = ['rgba(35,34,38,255)', 'rgba(120,70,11,255)', 'rgba(37,107,53,255)', 'rgba(37,101,148,255)',
+    'rgba(173,171,193,255)', 'rgba(234,101,157,255)', 'rgba(194,75,0,255)', 'rgb(197,200,78)'];
+   
+const palettePCarpenter = ['rgba(37,37,43,255)', 'rgba(87,135,101,255)', 'rgba(0,97,159,255)', 'rgba(117,100,104,255)', 'rgba(180,28,0,255)',
+    'rgba(215,110,4,255)', 'rgba(181,128,68,255)', 'rgba(168,153,93,255)', 'rgb(217,194,37,255)'];
+   
+const paletteItaly = ['rgba(26,28,21,255)', 'rgba(62,75,27,255)', 'rgba(103,93,78,255)', 'rgba(66,81,105,255)', 'rgba(26,73,88,255)', 'rgba(152,122,131,255)', 'rgba(213,116,106,255)',
+    , 'rgba(116,40,15,255)', 'rgba(232,96,0,255)', 'rgba(255,199,61,255)', 'rgba(220,192,38,255)', 'rgb(193,143,26,255)'];
+   
+   /*Function that changes the currentPalette*/
+   function updatePalette(paletteName){
+     if (paletteName === "Lapierre c1860-1900+") {
+        currentPaletteName = paletteName;
+        currentPalette = paletteLapierre;
+       } else if (paletteName === "W.E. & F. Newton c1852-57") {
+        currentPaletteName = paletteName;
+        currentPalette = paletteEFNewton;
+       } else if(paletteName === "Carpenter & Westley c1838-1900+"){
+        currentPaletteName = paletteName;
+        currentPalette = paletteCarpenterWestley;
+       }else if(paletteName === "P. Carpenter 1822-35"){
+        currentPaletteName = paletteName; 
+        currentPalette = palettePCarpenter;
+       }else{
+        currentPaletteName = paletteName; 
+        currentPalette = paletteItaly;
+       }
+   
+       displayPalette()
+   }
+
+   /*Function that changes the colors in the palette used to paint based on currentPalette*/
+   function displayPalette() {
+    
+    let leftPalette = document.getElementById('paletteContainer');
+
+    //Clear existing palette
+    leftPalette.innerHTML = '';
+
+    let paletteSelected = document.createElement('div');
+    paletteSelected.classList.add('palette-selected');
+
+    leftPalette.appendChild(paletteSelected);
+
+    let paletteColors = document.createElement('div');
+    paletteColors.classList.add('palette-selected-colors');
+
+    paletteSelected.appendChild(paletteColors);
+    
+    //Add each color of the palette chosen
+    currentPalette.forEach((c, index)=>{
+      let colorDiv = document.createElement('div');
+      colorDiv.style.backgroundColor = c;
+      colorDiv.classList.add('palette-selected-option');
+      paletteColors.appendChild(colorDiv);
+    })
+
+    // Add event listeners to each color div in the left palette container
+    let colorDivs = leftPalette.querySelectorAll('.palette-selected-option');
+    colorDivs.forEach((colorDiv) => {
+    colorDiv.addEventListener('click', function() {
+    
+    // Get the background color of the clicked color div
+    let c = colorDiv.style.backgroundColor;
+    let r = red(c);
+    let g = green(c);
+    let b = blue(c);
+    selectedColor.style.backgroundColor = c;
+    currentColor = color(r,g,b);
+    pg.stroke(currentColor)
+    document.getElementById('selectedColor').classList.remove("d-none");
+   });
+  });
+  document.getElementById('paletteContainer').classList.remove("d-none");
+}
